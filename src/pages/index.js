30dcbe10819179c35f08481b6500af8d5a1ba2db
Dan Haddigan <haddigan@gmail.com>
2020-09-03 15:46:06 -0400

    make the font smaller

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import EricImage from '../../images/x.png'
 
 export default () => (
-  <div>
-    <h1 style={{fontSize: "200px", font: 'Comic Sans'}}>Ric's Picks</h1>
+  <div style={{maxWidth: "100%"}}>
+    <h1 style={{fontSize: "130px", font: 'Comic Sans'}}>Ric's Picks</h1>
     <div>
       <img src="https://media.giphy.com/media/tHJGFW5NxCFyMoSubY/giphy.gif" />
       <img src={EricImage} />
