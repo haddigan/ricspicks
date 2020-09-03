23806809a8d51dcd5799c999ff11785e91d21416
Dan Haddigan <haddigan@gmail.com>
2020-09-03 15:59:00 -0400

    change the font

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import EricImage from '../../images/x.png'
 
 export default () => (
-  <div style={{maxWidth: "100%"}}>
+  <div style={{maxWidth: "100%", fontFamily: 'Grandstander'}}>
     <h1 style={{fontSize: "130px", font: 'Comic Sans'}}>Ric's Picks</h1>
     <div>
       <img src="https://media.giphy.com/media/tHJGFW5NxCFyMoSubY/giphy.gif" />
