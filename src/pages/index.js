b485d611362f3be2bdacbcd9f1bc7c5141e5a3ca
Dan Haddigan <haddigan@gmail.com>
2020-09-03 16:05:16 -0400

    images max-size

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,9 @@ export default () => (
   <div style={{maxWidth: "100%", fontFamily: 'Grandstander'}}>
     <h1 style={{fontSize: "130px", font: 'Comic Sans'}}>Ric's Picks</h1>
     <div>
-      <img src="https://media.giphy.com/media/tHJGFW5NxCFyMoSubY/giphy.gif" />
-      <img src={EricImage} />
-      <img src="https://media.giphy.com/media/TKRxCYjIecx7xWqRpb/giphy.gif" />
+      <img style={{maxWidth: "100%", width: "auto"}} src="https://media.giphy.com/media/tHJGFW5NxCFyMoSubY/giphy.gif" />
+      <img style={{maxWidth: "100%", width: "auto"}} src={EricImage} />
+      <img style={{maxWidth: "100%", width: "auto"}} src="https://media.giphy.com/media/TKRxCYjIecx7xWqRpb/giphy.gif" />
     </div>
   </div>
 );
