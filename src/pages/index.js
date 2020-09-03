6e89d2e267ac9c81055d94de89931a4006f27441
Dan Haddigan <haddigan@gmail.com>
2020-09-03 15:26:44 -0400

    add images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import EricImage from '../../images/x.png'
 
 export default () => (
   <div>
     <h1>Ric's Picks</h1>
     <div>
+      <img src={EricImage} />
       <img src="https://media.giphy.com/media/TKRxCYjIecx7xWqRpb/giphy.gif" />
     </div>
   </div>
