ce11c8f8b224e8cebfebe5f5835e101731fc431c
Dan Haddigan <haddigan@gmail.com>
2020-09-03 15:29:49 -0400

    add another image

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,9 @@ import EricImage from '../../images/x.png'
 
 export default () => (
   <div>
-    <h1>Ric's Picks</h1>
+    <h1 style={{fontSize: "200px", font: 'Comic Sans'}}>Ric's Picks</h1>
     <div>
+      <img src="https://media.giphy.com/media/tHJGFW5NxCFyMoSubY/giphy.gif" />
       <img src={EricImage} />
       <img src="https://media.giphy.com/media/TKRxCYjIecx7xWqRpb/giphy.gif" />
     </div>
