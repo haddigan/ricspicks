9eb372ce3e0e799c0298e69699d825cb02492fd1
Dan Haddigan <haddigan@gmail.com>
2020-09-03 14:18:01 -0400

    make a new thing

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => (
+  <div>
+    <h1>Ric's Picks</h1>
+    <div>
+      <img src="https://media.giphy.com/media/TKRxCYjIecx7xWqRpb/giphy.gif" />
+    </div>
+  </div>
+);
