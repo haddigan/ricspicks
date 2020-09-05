9f7ae0d6b629dfb36d37dfcf80660a9f2e1e8989
Dan Haddigan <haddigan@gmail.com>
2020-09-05 18:12:17 -0400

    add derby picks

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import EricImage from '../../images/x.png'
+import DerbyPicks from '../../images/IMG_0162.png'
 
 export default () => (
   <div style={{maxWidth: "100%", fontFamily: 'Grandstander'}}>
     <h1 style={{fontSize: "130px", font: 'Comic Sans'}}>Ric's Picks</h1>
     <div>
+      <img style={{maxWidth: "100%", width: "auto"}} src={DerbyPicks} />
       <img style={{maxWidth: "100%", width: "auto"}} src="https://media.giphy.com/media/tHJGFW5NxCFyMoSubY/giphy.gif" />
       <img style={{maxWidth: "100%", width: "auto"}} src={EricImage} />
       <img style={{maxWidth: "100%", width: "auto"}} src="https://media.giphy.com/media/TKRxCYjIecx7xWqRpb/giphy.gif" />
