1a397151cc98a967b9b162ec45570deee9874ad9
Dan Haddigan <haddigan@gmail.com>
2020-09-05 19:34:31 -0400

    eric won

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import EricImage from '../../images/x.png'
 import DerbyPicks from '../../images/IMG_0162.png'
+import EricWin from '../../images/IMG_0163.png'
 
 export default () => (
   <div style={{maxWidth: "100%", fontFamily: 'Grandstander'}}>
     <h1 style={{fontSize: "130px", font: 'Comic Sans'}}>Ric's Picks</h1>
     <div>
+      <img style={{maxWidth: "100%", width: "auto"}} src={EricWin} />
       <img style={{maxWidth: "100%", width: "auto"}} src={DerbyPicks} />
       <img style={{maxWidth: "100%", width: "auto"}} src="https://media.giphy.com/media/tHJGFW5NxCFyMoSubY/giphy.gif" />
       <img style={{maxWidth: "100%", width: "auto"}} src={EricImage} />
