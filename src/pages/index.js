b41d0b6c330061310f2b87744272f96b43efd307
Dan <code@haddigan.email>
2021-05-01 11:49:21 -0400

    add derby picks

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,31 @@ import EricImage from '../../images/x.png'
 import DerbyPicks from '../../images/IMG_0162.png'
 import EricWin from '../../images/IMG_0163.png'
 import Stakes from '../../images/c.png'
+import EricCowboyHat from '../../images/bighat.jpg'
+import Derby from '../../images/derby.png'
 
 export default () => (
-  <div style={{ maxWidth: '100%', fontFamily: 'Grandstander' }}>
-    <h1 style={{ fontSize: '130px', font: 'Comic Sans' }}>Ric's Picks</h1>
+  <div
+    style={{
+      maxWidth: '100%',
+      fontFamily: 'Grandstander',
+      backgroundImage: `url(${EricCowboyHat})`,
+    }}
+  >
+    <h1 style={{ fontSize: '130px', font: 'Comic Sans', marginTop: '0' }}>
+      Ric's Picks
+    </h1>
     <div>
+      <img
+        style={{
+          maxWidth: '100%',
+          width: '90%',
+          padding: '5%',
+          display: 'inline-block',
+        }}
+        src={Derby}
+      />
+      <img style={{ maxWidth: '100%', width: 'auto' }} src={EricCowboyHat} />
       <img style={{ maxWidth: '100%', width: 'auto' }} src={Stakes} />
       <img style={{ maxWidth: '100%', width: 'auto' }} src={EricWin} />
       <img style={{ maxWidth: '100%', width: 'auto' }} src={DerbyPicks} />
