5e44d6342d6986a907a3aa398e60b5f1371f4645
Dan <code@haddigan.email>
2025-05-03 16:38:58 -0400

    kentucky 2025

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,7 @@ import Belmont22 from '../../images/Belmont2022.jpg'
 import Derby23_1 from '../../images/Derby2023.png'
 import Derby23_2 from '../../images/Derby2023-2.png'
 import Derby_24 from '../../images/IMG_6177.jpg'
+import Derby_25 from '../../images/IMG_1868.jpeg'
 
 export default () => (
   <div
@@ -38,36 +39,53 @@ export default () => (
           }}
         >
           <p>
+            <img src={Derby_25} alt="Kentucky Derby 20202025" />
+          </p>
+          <p>
+            Good day racers! I completely forgot today was the derby until I
+            woke up. Let’s get right to it. I don’t know much about these
+            horses, but what I do know if that I’m betting the house on my dog
+            Bob Baf. That’s right, everyone’s favorite silver fox horse doper is
+            back. With a horse named Citizen Bull at 13/1 right now. How could
+            you not bet? You kidding me??
+          </p>
+          <p>
+            Looks like this horse Journalism is favorite, but I think that’s a
+            dumbass name so I don’t like him, but I’ll still throw him in an
+            exacta with Citizen Bull.{' '}
+          </p>
+          As far as other horses go, I’m liking this Japanese horse Luxor Cafe,
+          he comes from a long line of winners, so maybe throw him in there too.
+          I don’t really care about long shots, just pick one, it won’t win.
+          Okay great, enjoy the race. I think I need to go down to Churchill
+          next year, anyone wanna go???
+          <p>~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~</p>
+          <p>
             GOOD DAY RACERS! How the hell are ya, you came here for picks, so
             I’m gonna get right to it. 1 word for you guys today, SIERRA! (Frank
             Ocean voice). That's right, Sierra Leone.  This horse is hotter than
             a 2 dollar pistol.  I’m going all in on this bad boy today.  
           </p>
-
           <p>
             As far as other horses, the favorite is obviously Fierceness at 7/2
             but other than that, its pretty wide open. Lots of long shots so
             take your pick.  I tend to hate a favorite, but will throw him into
             some exactas.
           </p>
-
           <p>
             I’m liking Catalytic who was the runner up behind Fierceness in the
             Florida derby. Could see him on the board today. Great name too. 
           </p>
-
           <p>
             Another pick I like is the horse Forever Young, clearly the fastest
             horse in the mix, could be interesting.
           </p>
-
           <p>
             I always like a #1 horse too, this young steed Dornoch has had some
             great races, his last one he couldn’t fully fire, but the other 3 he
             came in 1st. Maybe he just stumbled a bit. He’s also the full
             brother to last year's winner Mage.  Hmmm interesting.  
           </p>
-
           <p>
             Okay that’s it for today, best of luck everyone. Now take your mint
             julep, raise it to the horizon, and thank god you’re alive on this
